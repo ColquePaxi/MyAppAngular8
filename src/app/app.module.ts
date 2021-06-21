62d475d0a9d37dcbbde3e25c466944e60d7dcf05
valterp <valter.paxi@ext.hdi.com.br>
2021-06-21 16:00:37 -0300

    Material App: commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyFirstComponent } from './myfirst.component';
-import { MySecondComponent } from './my-second/my-second.component';
-import { MyThirdComponent } from './my-second/my-third/my-third.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyFirstComponent,
-    MySecondComponent,
-    MyThirdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
