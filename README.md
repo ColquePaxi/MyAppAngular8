4ec91219adeca205f7a7632cd5d3e7694f375122
valterp <valter.paxi@ext.hdi.com.br>
2021-06-26 11:30:27 -0300

    Seção 3 - Data Binding - Esqueleto do projeto

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MaterialApp
+# DataBindingApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.29.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
