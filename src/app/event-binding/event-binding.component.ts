835e90d31def2765ab0255e06fbf22ea9c9d52d5
valterp <valter.paxi@ext.hdi.com.br>
2021-06-26 16:54:28 -0300

    Seção 3 - 16. Event Binding - interação via evento

---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+  buttonName:String = "My Button";
+  i = 0;   
+  spinnerMode = "determinate";
+  btnEnable = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save() {
+     console.log("Click");
+  }
+
+  inc() {
+     this.i++;
+     this.buttonName = " It was clicked " + this.i + " times";
+  }
+
+  disable() {
+     this.btnEnable = false;
+     this.spinnerMode = "indeterminate";
+     setTimeout( () => {
+        this.btnEnable = true;
+        this.spinnerMode = "determinado";
+     }, 3000 );
+   }
+
+}
