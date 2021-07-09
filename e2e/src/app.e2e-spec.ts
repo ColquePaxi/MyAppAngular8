314f975300ae3fc656c47e74b036f888e7143527
valterp <valter.paxi@ext.hdi.com.br>
2021-07-09 11:39:02 -0300

    Seção 4.Diretivas - 23. ngstyle

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('data-binding-app app is running!');
+    expect(page.getTitleText()).toEqual('directives-app app is running!');
   });
 
   afterEach(async () => {
