2842cacf9bb58e6a88ecf49a38bb7f77a8b1172c
valterp <valter.paxi@ext.hdi.com.br>
2021-07-09 12:15:05 -0300

    Seção 4.Diretivas - 23.ngstyle - aplicado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule,
+         MatSelectModule, 
+         MatRadioModule 
+      } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
